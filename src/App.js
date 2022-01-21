c105bcd4753e42394899ffb40086d764a6ee309c
okiki123 <okikimalomo17@gmail.com>
2022-01-21 22:17:07 +0100

    Updated Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import Topbar from './components/topbar/Topbar'
+import Home from './pages/Home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Settings from './pages/settings/Settings';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
-  return (
+  const user = false;
+  return ( <Router>
     <Topbar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/register" element={user ? <Home /> : <Register /> } />
+      <Route path="/login" element={user ? <Home /> : <Login />} />
+      <Route path="/write" element={user ? <Write /> : <Register />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/post/:postId" element={<Single />} />
+    </Routes>
+    
+    </Router>
   );
 }
 
