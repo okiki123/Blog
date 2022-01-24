e7e9cb0144eaf2527cf595be354685458b21078b
okiki123 <okikimalomo17@gmail.com>
2022-01-24 23:39:25 +0100

    Updated Commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,10 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { Context } from './context/Context';
+import { useContext } from "react";
 function App() {
-  const user = false;
+  const { user } = useContext(Context);
   return ( <Router>
     <Topbar />
     <Routes>
